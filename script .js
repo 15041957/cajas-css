83317ad6a229a3842ff4936cda7992e109909f20
15041957 <urdaheredia@gmail.com>
2024-11-19 10:28:28 +0100

    creado js

---
+++ script .js
@@ -0,0 +1,68 @@
+/*
+alert ("buenos dias"); 
+alert(1+9+41);
+alert("1+9+41 ");
+//aleralert("1+9+41 ");
+//comentario//
+// crear variable//
+let saludo = "buenas";
+//rellenar variable//
+let  buenas= "adios";
+saludo = "k tal";
+//alert //
+alert( saludo);
+let adios = "adios";
+// igualar una variable a otra //
+saludo ="adios";
+alert (1+2);
+// sumar 
+alert (1-2);
+//restar
+alert (8+12/2);
+//dividir
+alert (8*2);
+//mulriplicar
+*/
+
+let edad= 16;
+edad = 17;
+edad +1;
+edad --;
+console.log(edad);
+
+
+//muestra lo que pongamos enla console
+
+//alert muestra una ventana emergente 
+let nombreUsuario= "Rosa";
+//no se puede cambiar el valor de las constantes
+const SALUDO_NOMBREUSUARIO = "hola "+ nombreUsuario; //"adios Roxana"
+// operadores logicos 
+console.log(5>4); //mayor que
+let saludo_nombreUsuario
+let texto;
+let texto1="fiesta";
+let habitacion= "azul";
+console.log("fiesta");
+console.log("habitacion");
+
+
+console.log(5<8);// menor que 
+console.log(5>=6);//mayor o igual
+console.log(5<=7);// menor o igual 
+console.log(5==5);// igual que 
+console.log(5!=7);// no es igual 
+
+//condicionales "if" siempre
+// recojo el elemento // que hago 
+
+if (edad>=18);{
+
+}
+//interactuar con htm DOM
+//recojo el elemento // que hago 
+document.getElementById("texto").innerHTML="texto";
+document.getElementsByClassName("parrafo")
+
+
+

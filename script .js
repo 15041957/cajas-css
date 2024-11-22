9b0efb07bd4444c535bf9c65ce5859bba5e85edc
15041957 <urdaheredia@gmail.com>
2024-11-22 10:38:42 +0100

    cajas imagenes

--- script .js
+++ script .js
@@ -24,45 +24,91 @@ alert (8*2);
 //mulriplicar
 */
 
-let edad= 16;
+let edad = 16;
 edad = 17;
-edad +1;
-edad --;
+edad++; //sumar 1
+edad--;// restar 1
 console.log(edad);
 
 
 //muestra lo que pongamos enla console
 
 //alert muestra una ventana emergente 
-let nombreUsuario= "Rosa";
+let nombreUsuario = "Rosa";
 //no se puede cambiar el valor de las constantes
-const SALUDO_NOMBREUSUARIO = "hola "+ nombreUsuario; //"adios Roxana"
-// operadores logicos 
-console.log(5>4); //mayor que
+const SALUDO_NOMBREUSUARIO = "hola " + nombreUsuario; //"adios Roxana"
+
 let saludo_nombreUsuario
-let texto;
-let texto1="fiesta";
-let habitacion= "azul";
-console.log("fiesta");
-console.log("habitacion");
+let texto = "mis cajas";
+let texto1 = "fiesta";
+let habitacion = "azul";
 
-
-console.log(5<8);// menor que 
-console.log(5>=6);//mayor o igual
-console.log(5<=7);// menor o igual 
-console.log(5==5);// igual que 
-console.log(5!=7);// no es igual 
+//operadores logicos
+console.log(5 > 4); //mayor que
+console.log(5 < 8);// menor que 
+console.log(5 >= 6);//mayor o igual
+console.log(5 <= 7);// menor o igual 
+console.log(5 == 5);// igual que 
+console.log(5 != 7);// no es igual 
+console.log(texto1);
+console.log(habitacion);
+console.log(texto);
 
 //condicionales "if" siempre
 // recojo el elemento // que hago 
 
-if (edad>=18);{
+if (edad >= 18); {
 
 }
+
 //interactuar con htm DOM
 //recojo el elemento // que hago 
-document.getElementById("texto").innerHTML="texto";
+document.getElementById("texto").innerHTML = ("hola");
+
 document.getElementsByClassName("parrafo")
+document.body.style.color = "red";
+
+// funciones, van almacenar 
+function saludar() {
+    alert("bienbenido");
+}
+//llamada
+function modoNoche() {
+    document.body.style.color = "white";
+    document.body.style.backgroundColor = "black";
+
+}
+
+function modoDia() {
+    document.body.style.color = "black";
+    document.body.style.backgroundColor = "white";
+
+    //
+}
+//seleciono para  primer elemento 
+// selectores 
+document.body.querySelector("texto").innerHTML="metiendo el codigo en p";
+document.querySelector("p").innerHTML= ""
+//selecciono  todos los elementos 
+//document.queryselectoAll
+
+let texto2 = document.body.querySelector("texto");
+texto2.innerHTML="guardado en mi variable";
+ // para guardar los archivos y tener que poner de nuevo la busqueda 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
